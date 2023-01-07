9eb6202c5b3c540befbc5ac3edf374cd5f7c68cd
carron10 <carronmuleya10@gmail.com>
2023-01-07 15:54:05 +0200

    SLla

--- js/comments.js
+++ js/comments.js
@@ -1,7 +1,7 @@
 
 $(function () {
     var url = location.pathname;
-    $.get("/countcomments",{url: url}).done(function(d){
+    $.get("chatsfly.co.zw/countcomments",{url: url}).done(function(d){
         $("#view_comments i").text(d);
     });
     function subl(){
@@ -73,7 +73,7 @@ $(function () {
                 }
             });
         }
-        $.getJSON("/comments", {url: url}).done(function (data) {
+        $.getJSON("chatsfly.co.zw/comments", {url: url}).done(function (data) {
             render(data);
             if (connect === '') {
                 $("#comments .comment-section").html('<div class="text-center p-4">No comments so far, Be the first to comment <i class="fa  fa-frown-o"></i></div>');
--- js/like.js
+++ js/like.js
@@ -1,6 +1,6 @@
 $(function () {
     var u = location.pathname;
-    $.get("/like", {url: u}).done(function (d) {
+    $.get("chatsfly.co.zw/like", {url: u}).done(function (d) {
         $("#likee").html(d);
         $("#likee .btn").click(function () {
             var b = $(this), l = b.attr("id");
